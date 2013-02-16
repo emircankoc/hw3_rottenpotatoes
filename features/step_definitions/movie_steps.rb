151471de55de4690d76b06d78e7ca3b2bf612de4
emircankoc <koc.emircan@gmail.com>
2013-02-16 10:38:23 -0800

    Nearly done at filtering feature

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create! movie # Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +25,47 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  
+  rating_list.split.each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings_#{rating}"}  
+    else
+      step %Q{I check "ratings_#{rating}"}
+    end
+  end
+end
+
+Then /I should (not )?see movies rated: (.*)/ do |not_see, rating_list|
+  #  page.body is the entire content of the page as a string.
+  list = Movie.find_all_by_rating(rating_list.split)
+
+  list.each do |movie|
+    if not_see
+      (page.body.match /#{movie.title}/).should be_false
+    else
+      (page.body.match /#{movie.title}/).should be_true
+    end
+  end
+end
+
+When /I (un)?check all of the ratings/ do |uncheck|
+  print uncheck
+  Movie.all_ratings.each do |rating|
+    print rating
+    if uncheck
+      step %Q{I uncheck "ratings_#{rating}"}  
+    else
+      step %Q{I check "ratings_#{rating}"}
+    end
+  end
+end
+
+Then /I should (.*) of the movies/ do |see_all|
+  if see_all == "see all"
+    print Movie.all.size
+    (Movie.all.size == 10).should be_true
+  else
+    print Movie.all.size
+    #(Movie.all.size == 0).should be_true
+  end
 end
